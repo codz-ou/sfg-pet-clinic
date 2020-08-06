d8a7dd99f4e735f41ba4660b22463d992c22a386
codz-ou <codz.ou@gmail.com>
2020-08-06 13:19:55 +0300

    update: bug fix on scm Connetion

--- pet-clinic-data/src/main/java/com/codz/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-data/src/main/java/com/codz/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.codz.sgfpetclinic;
+package com.codz.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
