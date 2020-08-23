907633336c2b12d0714a92f5b50434b76442c92d
codz-ou <codz.ou@gmail.com>
2020-08-23 04:47:44 +0300

    added Base Entity(JPA). Closes #35

--- pet-clinic-data/src/main/java/com/codz/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/codz/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,16 @@
 package com.codz.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -14,5 +20,9 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+
+
+
 
 }
