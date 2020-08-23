0942d9bdd15a738284176311059b3975c92268d6
codz-ou <codz.ou@gmail.com>
2020-08-23 10:20:04 +0300

    implemented Spring Data JPA Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/com/codz/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/codz/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.codz.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findOwnerByLastName(String lastName);
 }
